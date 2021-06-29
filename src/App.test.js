8d53795af127b5d4c8450325c8a81b12d8a9b5fc
Scott Campbell <thisisscottcampbell@gmail.com>
2021-06-29 13:44:39 -0400

    implemented test for App internal componets

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import EmojiResults from './EmojiResults';
+import SearchInput from './SearchInput';
+import Header from './Header';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+});
+
+it('contains Header, SearchInput, EmojiResults', () => {
+	const wrapper = shallow(<App />);
+
+	expect(wrapper.find(EmojiResults).length).toEqual(1);
+	expect(wrapper.find(SearchInput).length).toEqual(1);
+	expect(wrapper.find(Header).length).toEqual(1);
 });
