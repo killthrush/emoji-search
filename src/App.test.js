0975506c0626f3c64d7b5972bb41da137671d7ae
Ryan <ryan@Ryans-MacBook-Pro.local>
2021-07-08 13:30:03 -0400

    Add additional tests to ensure proper rendering

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
+
+it("should render a div with class component-search-input", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  const inputContainer = document.getElementsByClassName("component-search-input");
+  expect(inputContainer).toBeTruthy();
+});
